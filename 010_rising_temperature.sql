ed40a148d7d84f43476170d49cf1488091d03646
Bhavya Parmar <46936806+BhavyaSParmar@users.noreply.github.com>
2025-05-15 11:31:55 -0400

    Update 010_rising_temperature.sql

--- 010_rising_temperature.sql
+++ 010_rising_temperature.sql
@@ -4,4 +4,4 @@ select curr.id as Id
 from weather curr
 join weather prev
 on datediff (curr.recordDate, prev.recordDate) = 1
-where curr.temperature > prev.temperature
+where curr.temperature > prev.temperature     

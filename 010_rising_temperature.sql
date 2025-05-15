67a2bb3494fd73074f885d000384fd848eee0917
BHAVYAPARMARR <bsparrmar21@gmail.com>
2025-05-15 11:29:40 -0400

    Create 010_rising_temperature.sql

---
+++ 010_rising_temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+select curr.id as Id
+from weather curr
+join weather prev
+on datediff (curr.recordDate, prev.recordDate) = 1
+where curr.temperature > prev.temperature
